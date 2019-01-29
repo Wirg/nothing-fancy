e6b8d2dcb6fbad0c7f4abb425eeaae75b6b5e24b
Arnault Chazareix <arnaultc@sicara.com>
2019-01-29 18:47:43 +0100

    improving job order

--- scripts/run_first_job.sh
+++ scripts/run_first_job.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+job_dir=jobs
 extension=job
 initial_path=`pwd`
-first_job=`/bin/ls jobs/*.${extension} | head -n 1`
+first_job=${job_dir}/`/bin/ls ${job_dir} | grep -P ".*.job" | sort -g | head -n 1`
 command=`cat ${first_job}`
 tmp_name=${first_job}~${WORKER_NAME}
 mv ${first_job} ${tmp_name}
