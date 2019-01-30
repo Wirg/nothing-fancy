ce59e2876e79843f2ff7e820874abef61c8572c3
Arnault Chazareix <arnaultc@sicara.com>
2019-01-30 13:37:32 +0100

    adding date and priority in commit name

--- scripts/new_job.sh
+++ scripts/new_job.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 commit=`git rev-parse HEAD`
-echo git checkout ${commit} "&&" "$@" > jobs/_new_job_${commit}.job
+time=`date +%Y%m%d_%H%M%S`
+
+#!/bin/sh
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize variables:
+file_header=''
+
+while getopts "p?:" opt; do
+    case "$opt" in
+    p)
+        file_header=_${file_header}
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[[ "${1:-}" = "--" ]] && shift
+
+args=$@
+
+echo git checkout ${commit} "&&" "$@" > jobs/${file_header}${time}.job
