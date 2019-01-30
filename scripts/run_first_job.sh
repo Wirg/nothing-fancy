d7439d5fd399f49ee1964667985a0885865afa5a
Arnault Chazareix <arnaultc@sicara.com>
2019-01-30 20:17:08 +0100

    improving jobs done management

--- scripts/run_first_job.sh
+++ scripts/run_first_job.sh
@@ -2,11 +2,21 @@
 job_dir=jobs
 extension=job
 initial_path=`pwd`
-first_job=${job_dir}/`/bin/ls ${job_dir} | grep -P ".*.job" | sort -g | head -n 1`
+# hello.job
+first_job=`/bin/ls ${job_dir} | grep -P ".*.job" | sort -g | head -n 1`
+# hello
+job_name=${first_job%.*}
+# jobs/hello.job
+first_job=${job_dir}/${first_job}
 command=`cat ${first_job}`
 tmp_name=${first_job}~${WORKER_NAME}
+# jobs/hello.job~worker_i
 mv ${first_job} ${tmp_name}
-echo Running ${first_job}
+
+echo Running ${job_name}
+
 bash ${initial_path}/${tmp_name}
 rm ${tmp_name}
-echo ${tmp_name} | xargs basename | grep -oP "\w+."${extension} >> jobs_done
+
+# Dumping
+echo ${job_name} >> jobs_done
