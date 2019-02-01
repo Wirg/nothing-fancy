f68a10d717340587c6d43bd94a5b2cd106afba4b
Arnault Chazareix <arnaultc@sicara.com>
2019-02-01 17:31:56 +0100

    checking gpu

--- scripts/run_job_if_available.sh
+++ scripts/run_job_if_available.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 job_dir=${BASE_DIRECTORY}/jobs
 number_of_jobs=`/bin/ls ${job_dir} | grep -P ".*.job" | wc -l`
-if [ $number_of_jobs -gt 0 ]
+number_of_gpu_jobs=`nvidia-smi | grep "python" | grep -P "\d{4,}MiB" | wc -l`
+
+if [ $number_of_jobs -gt 0 ] && [ $number_of_gpu_jobs -gt 0 ]
 then
     bash ${BASE_DIRECTORY}/scripts/run_first_job.sh
 fi
