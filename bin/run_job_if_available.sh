ea36966b1d777a79f3a56536627a2d98fda66f16
Arnault Chazareix <arnaultc@sicara.com>
2019-02-04 15:30:52 +0100

    removing echo

--- bin/run_job_if_available.sh
+++ bin/run_job_if_available.sh
@@ -4,8 +4,6 @@ source $(dirname "$0")/base_config.sh
 number_of_jobs=$(/bin/ls "${job_dir}" | grep -cP ".*.job$")
 number_of_gpu_jobs=$(nvidia-smi | grep "python" | grep -cP "\d{4,}MiB")
 
-echo ${number_of_jobs}
-
 if [ "$number_of_gpu_jobs" -eq 0 ]
 then
 if [ "$number_of_jobs" -gt 0 ]
