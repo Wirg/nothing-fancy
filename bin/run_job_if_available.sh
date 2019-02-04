12662c70694a657dc3941aab1c6f5165cb91c708
Arnault Chazareix <arnaultc@sicara.com>
2019-02-04 15:29:37 +0100

    invertion condition orders for clarity

--- bin/run_job_if_available.sh
+++ bin/run_job_if_available.sh
@@ -6,14 +6,14 @@ number_of_gpu_jobs=$(nvidia-smi | grep "python" | grep -cP "\d{4,}MiB")
 
 echo ${number_of_jobs}
 
-if [ "$number_of_jobs" -gt 0 ]
-then
 if [ "$number_of_gpu_jobs" -eq 0 ]
+then
+if [ "$number_of_jobs" -gt 0 ]
 then
     bash "${BASE_DIRECTORY}"/bin/run_first_job.sh
 else
-    echo "GPU is not available"
+    echo "No job left"
 fi
 else
-    echo "No job left"
+    echo "GPU is not available"
 fi
